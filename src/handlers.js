6dfce45401e75708ac3293735be84fdd127da08c
Alexis <alexclaustro@gmail.com>
2021-05-01 16:28:12 -0500

    Minor code refactoring.

--- src/handlers.js
+++ src/handlers.js
@@ -1,42 +1,24 @@
-const weapons = require('./weapons')
-const { generateRandomNumber, commands, processCommands, prefix } = require('./utils')
-
+const { buildMessage, commands, processCommands, prefix } = require('./utils')
 
 const handleMessage = (message) => {
     if(message.author.bot) return
     if(message.content.startsWith(prefix)){
         const [command, ...args] = processCommands(message)
         command.toLowerCase()
-        if(command === commands.HUNT){
-            generateRandomWeapon(args, message)
+        switch(command){
+            case commands.HUNT:
+                generateRandomWeapon(args, message)
+                break
+            default:
+                message.channel.send('Commando no valido = (')
         }
     }
 }
 
 const generateRandomWeapon = (args, message) => {
-    let players = 1
-    console.log(args)
-    console.log(args.length)
-    if(args.length === 0) {
-        args.push(4) 
-    }
-    try{
-        players = parseInt(args[0])
-        if(players > 4) players = 4
-        if(players < 0) players = 4
-        for(let i=1; i<=players; i++){
-            const number = generateRandomNumber()
-            const response = `Jugador ${i} le toco usar ${weapons[number]}`
-            console.log(response)
-            message.channel.send(response)
-        }
-    }catch{
-        console.log('not a number')
-        message.channel.send('El comando hunt necesita un numero de jugadores entre 1 y 4. Ej, !hunt 3')
-    }
+    const response = buildMessage(args)
+    message.channel.send(response)
 }
-
-
 
 module.exports = {
     handleMessage
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const weapons = require('./weapons')
+
 const prefix = '!'
 
 const generateRandomNumber = () => {
@@ -15,9 +17,33 @@ const commands = {
     HUNT: 'hunt'
 }
 
+const buildMessage = (args) => {
+    let players = 1
+    let response = ''
+    console.log(args)
+    if(args.length === 0) {
+        args.push(4) 
+    }
+    players = parseInt(args[0])
+    console.log(players)
+    if(!isNaN(players)){
+        if(players > 4) players = 4
+        if(players < 0) players = 4
+        for(let i=1; i<=players; i++){
+            const number = generateRandomNumber()
+            response += `Jugador ${i} le toco usar ${weapons[number]}\n`
+            console.log(response)
+        }
+    }else{
+        response = 'El comando hunt necesita un numero de jugadores entre 1 y 4. Ej, !hunt 3'
+    }
+    return response 
+}
+
 module.exports = {
     generateRandomNumber,
     processCommands,
     commands,
-    prefix
+    prefix,
+    buildMessage
 }
